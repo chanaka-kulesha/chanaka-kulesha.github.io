b153b979daa68a8ac1eab38762e67940c4faf9e3
Chanaka Sandaruwan <csgamage95@gmail.com>
2024-12-11 15:54:59 +0530

    Update video and appearance

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ audioElement.load()
 audioElement.addEventListener("canplay", function() { 
   audioElement.play(); 
 }, true);
+
+document.querySelectorAll('.navbar-nav .nav-link').forEach(function(anchor) {
+  anchor.addEventListener('click', function () {
+      var navbarCollapse = document.getElementById('navbarNav');
+      var bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+          toggle: false
+      });
+      bsCollapse.hide();  // Manually close the navbar after clicking a link
+  });
+});
